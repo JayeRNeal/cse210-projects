010163a7716206c217223176641e19e8754ec33b
Use your GitHub username <103981244+JayeRNeal@users.noreply.github.com>
2023-01-09 12:21:57 -0700

    finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Console.WriteLine("Weclome to the game of all games, Guess the magic number!");
+       Random randomGenerator = new Random();
+       int magicNumber = randomGenerator.Next(1,100);
+
+       int guess = -1;
+
+       while (guess != magicNumber)
+       {
+           Console.Write("What is your guess? ");
+           guess = int.Parse(Console.ReadLine());
+    
+            if (magicNumber > guess) 
+        {
+                Console.WriteLine("Higher");
+        }
+            else if (magicNumber < guess)
+        {
+                Console.WriteLine("Lower");
+        }
+            else
+        {
+                Console.WriteLine("Congradulations, you are wicked smart.");
+        }
+       }
     }
 }
